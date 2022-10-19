fceb251c4856f1b06eff7a9fa03c438dd1d4b4d7
Felix Therrien <felix.therrien@gmail.com>
2022-10-19 16:57:42 -0400

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='didgen',
+      version='0.0.0',
+      description='Generate molecules with requested properties',
+      url='https://github.com/ftherrien/inv-design',
+      author='Felix Therrien',
+      author_email='felix.therrien@gmail.com',
+      license='MIT',
+      packages=['didgen'],
+      python_requires='>=3',
+      install_requires=[
+          'numpy',
+          'torch',
+          'tqdm',
+          'matplotlib',
+          'torch_geometric',
+          'rdkit',
+      ],
+      scripts=['didgenerate'],)
