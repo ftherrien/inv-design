86d1f63e0cafb42cdf52976037cfb88ac37a00c4
Félix Therrien <felix.therrien@gmail.com>
2022-11-14 18:54:52 -0500

    Made code compatible with large datasets
    1. prepare_model_vector was not efficient for large datasets and was by far
    the limitting step. Updated it to be almost fully vectorial (could be be
    made fully vectorial with a bit more effort)
    
    2. Added with torch.no_grad() to free up some memory and no waste
       computation when model is purely evaluated

--- didgen/train.py
+++ didgen/train.py
@@ -6,6 +6,7 @@ import numpy as np
 from torch_geometric.loader import DataLoader
 import matplotlib.pyplot as plt
 import os
+import time
 
 def prepare_data(data, N, n_onehot):
     """Create explicit adjacency matrix and feature matrix from single data point"""
@@ -26,31 +27,41 @@ def prepare_data(data, N, n_onehot):
     
     return atom_fea, adj
 
-def prepare_data_vector(data, N, n_onehot):
+def prepare_data_vector(data, N, n_onehot, shuffle=False):
     """Create explicit adjacency matrix and feature matrix vector from mini-batch"""
+
+    t = time.time()
     
+    if shuffle:
+        p = torch.randperm(N)
+    else:
+        p = torch.arange(N)
+        
     atom_fea = 1*data.x[:,:n_onehot]
 
     atom_fea = torch.cat([atom_fea, torch.zeros(atom_fea.shape[0],1, device=atom_fea.device), torch.matmul(atom_fea[:,:n_onehot], torch.tensor([[1.0/8],[4.0/8],[5.0/8],[6.0/8],[7.0/8]], device=atom_fea.device))],dim=1)
     
     #N = torch.max(torch.tensor([torch.sum(data.batch==i) for i in range(data.num_graphs)]))
     
-    crystal_atom_idx = []
     new_atom_fea = torch.zeros(data.num_graphs, N, atom_fea.shape[1], device=atom_fea.device)
-    for i in range(data.num_graphs):
-        crystal_atom_idx.append(torch.where(data.batch==i)[0])
-        new_atom_fea[i,:len(crystal_atom_idx[-1]),:] = atom_fea[crystal_atom_idx[-1],:]
-        new_atom_fea[i,len(crystal_atom_idx[-1]):,n_onehot] = 1
-    
-    adj = torch.zeros(data.num_graphs, N,N, device=atom_fea.device)
-    for n,(i,j) in enumerate(data.edge_index.T):
+        
+    switch_points = torch.where(data.batch[1:] - data.batch[:-1] == 1)[0] + 1
 
-        nn = data.batch[i]
-        ii = i - crystal_atom_idx[nn][0]
-        jj = j - crystal_atom_idx[nn][0]
+    new_atom_fea_pieces = torch.tensor_split(atom_fea, switch_points.cpu())
+
+    # This can be vectorized: https://stackoverflow.com/questions/43146266/convert-list-of-lists-with-different-lengths-to-a-numpy-array
+    for i, ten in enumerate(new_atom_fea_pieces):
+        new_atom_fea[i,p[:ten.shape[0]],:] = ten
+        new_atom_fea[i,p[ten.shape[0]:],n_onehot] = 1
         
-        adj[nn,ii,jj] = data.edge_attr[n,:].matmul(torch.tensor([1,2,3,1.5], device=atom_fea.device))
-        
+    adj = torch.zeros(data.num_graphs, N,N, device=atom_fea.device)
+    bond_type = torch.tensor([1,2,3,1.5], device=atom_fea.device)
+    
+    nn = data.batch[data.edge_index.T[:,0]]
+    ij = data.edge_index.T - torch.cat([torch.zeros(1, dtype=torch.long, device=atom_fea.device), switch_points])[nn].unsqueeze(1).expand(data.edge_index.shape[1], 2)
+    
+    adj[nn,p[ij[:,0]], p[ij[:,1]]] = data.edge_attr.matmul(bond_type)
+    
     return new_atom_fea, adj
 
 def prepare_data_from_features(features, adj, n_onehot):
@@ -84,13 +95,15 @@ def train(qm9, config, output):
     model = CGCNN(config.n_onehot+2,
                      atom_fea_len=64, n_conv=3, h_fea_len=128, n_h=1,
                      classification=False).to(device)
-    
-    if config.use_pretrained:
+
+    if config.use_pretrained or config.transfer_learn:
         if os.path.isfile(output+'/model_weights.pth'):
-            model.load_state_dict(torch.load(output+'/model_weights.pth'))
+            model.load_state_dict(torch.load(output+'/model_weights.pth').to(device))
         else:
-            raise RuntimeError("Trying to use pretrained model but %s/model_weights.pth does not exist"%(output)) 
-    else:
+            raise RuntimeError("Trying to use pretrained model but %s/model_weights.pth does not exist"%(output))
+    
+    if not config.use_pretrained:
+        
         print("Size of database:", len(qm9))
     
         qm9_loader_train = DataLoader(qm9[:config.n_data], batch_size = config.batch_size, shuffle = True)
@@ -120,6 +133,7 @@ def train(qm9, config, output):
         # Train Network
         epoch_loss_train = []
         epoch_loss_valid = []
+        training_time = time.time()
         for epoch in range(config.num_epochs):
     
             model.train()
@@ -130,22 +144,22 @@ def train(qm9, config, output):
             for batch_idx, data in enumerate(qm9_loader_train):
                 # Get data to cuda if possible
                 data = data.to(device=device)
-        
-                inputs = prepare_data_vector(data, config.max_size, config.n_onehot)
+                
+                inputs = prepare_data_vector(data, config.max_size, config.n_onehot, shuffle=True)
     
                 inputs = nudge(*inputs, config.noise_factor) # Make the model more tolerent of non integers
                 
                 # forward
                 scores = model(*inputs).squeeze()
+                
                 target = data.y[:,3]-data.y[:,2]
     
-                epoch_scores.append(scores)
+                epoch_scores.append(scores.detach())
                 epoch_targets.append(target)
                 
                 loss = criterion(scores, target)
                 epoch_loss_train[-1] += float(loss)
-        
-                       
+                
                 # backward
                 optimizer.zero_grad()
                 loss.backward()
@@ -154,23 +168,23 @@ def train(qm9, config, output):
                 optimizer.step()
     
             model.eval()
+            with torch.no_grad():
+                epoch_loss_valid.append(0)
+                epoch_scores_valid = []
+                epoch_targets_valid = []
+                for batch_idx, data in enumerate(qm9_loader_valid):
+                    # Get data to cuda if possible
+                    data = data.to(device=device)
+                    
+                    # forward
+                    scores_valid = model(*prepare_data_vector(data, config.max_size, config.n_onehot)).squeeze()
+                    target_valid = data.y[:,3]-data.y[:,2]
                 
-            epoch_loss_valid.append(0)
-            epoch_scores_valid = []
-            epoch_targets_valid = []
-            for batch_idx, data in enumerate(qm9_loader_valid):
-                # Get data to cuda if possible
-                data = data.to(device=device)
-                
-                # forward
-                scores_valid = model(*prepare_data_vector(data, config.max_size, config.n_onehot)).squeeze()
-                target_valid = data.y[:,3]-data.y[:,2]
-    
-                epoch_scores_valid.append(scores_valid)
-                epoch_targets_valid.append(target_valid)
-                
-                loss = criterion(scores_valid, target_valid)
-                epoch_loss_valid[-1] += float(loss)
+                    epoch_scores_valid.append(scores_valid)
+                    epoch_targets_valid.append(target_valid)
+                    
+                    loss = criterion(scores_valid, target_valid)
+                    epoch_loss_valid[-1] += float(loss)
                 
             epoch_loss_train[-1] = epoch_loss_train[-1]/len(qm9_loader_train)
             epoch_loss_valid[-1] = epoch_loss_valid[-1]/len(qm9_loader_valid)
@@ -189,26 +203,54 @@ def train(qm9, config, output):
                 ax2.set_aspect("equal","box")
                 plt.pause(0.1)
 
-        torch.save(model.state_dict(), output+'/model_weights.pth')
+        print("Total training time:", time.time() - training_time)
 
-        model.eval()
+        print("Memoire", torch.cuda.memory_allocated(device))
+        
+        torch.save(model.state_dict(), output+'/model_weights.pth')
         
         plt.ioff()
-        plt.savefig("progress.png")
+        plt.savefig(output+"/progress.png")
+
+        model.eval()
+        with torch.no_grad():
+            epoch_scores = []
+            epoch_targets = []
+            for batch_idx, data in enumerate(qm9_loader_train):
+                # Get data to cuda if possible
+                data = data.to(device=device)
+                
+                inputs = prepare_data_vector(data, config.max_size, config.n_onehot, shuffle=True)
+                            
+                scores = model(*inputs).squeeze()
+                target = data.y[:,3]-data.y[:,2]
+                
+                epoch_scores.append(scores)
+                epoch_targets.append(target)
+                            
+            if config.num_epochs == 0:
+                epoch_scores_valid = []
+                epoch_targets_valid = []
+                for batch_idx, data in enumerate(qm9_loader_valid):
+                    # Get data to cuda if possible
+                    data = data.to(device=device)
+                
+                    # forward
+                    scores_valid = model(*prepare_data_vector(data, config.max_size, config.n_onehot)).squeeze()
+                    target_valid = data.y[:,3]-data.y[:,2]
+            
+                    epoch_scores_valid.append(scores_valid)
+                    epoch_targets_valid.append(target_valid)
 
         plt.figure()
+                
+        plt.plot(torch.cat(epoch_targets).cpu(), torch.cat(epoch_scores).cpu().detach().numpy(), ".")
         
-        criterion = nn.MSELoss()
+        print("FINAL TRAIN RMSE", criterion(torch.cat(epoch_targets), torch.cat(epoch_scores)))
         
-        data_train = next(iter(DataLoader(qm9[:config.n_data], batch_size = config.n_data, shuffle = False))).to(device=device)
-        plt.plot(data_train.y[:,4].cpu(), model(*prepare_data_vector(data_train, config.max_size, config.n_onehot)).cpu().detach().numpy(), ".")
+        plt.plot(torch.cat(epoch_targets_valid).cpu(), torch.cat(epoch_scores_valid).cpu().detach().numpy(),".")
         
-        print("FINAL TRAIN RMSE", criterion(data_train.y[:,4], model(*prepare_data_vector(data_train, config.max_size, config.n_onehot)).squeeze()))
-        
-        data_valid = next(iter(DataLoader(qm9[config.n_data:config.n_data + config.n_data//10], batch_size = config.n_data//10, shuffle = False))).to(device=device)
-        plt.plot(data_valid.y[:,4].cpu(), model(*prepare_data_vector(data_valid, config.max_size, config.n_onehot)).cpu().detach().numpy(),".")
-        
-        print("FINAL TEST RMSE", criterion(data_valid.y[:,4], model(*prepare_data_vector(data_valid, config.max_size, config.n_onehot)).squeeze()))
+        print("FINAL TEST RMSE", criterion(torch.cat(epoch_targets_valid), torch.cat(epoch_scores_valid)))
         
         ax = plt.gca()
         
