784b9993ca251285a815a469fbd68a7dba6d4eeb
Felix Therrien <felix.therrien@gmail.com>
2023-06-20 23:34:47 -0400

    Added find_packages() to setup for submodules

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='didgen',
       version='0.0.0',
@@ -7,7 +7,7 @@ setup(name='didgen',
       author='Felix Therrien',
       author_email='felix.therrien@gmail.com',
       license='MIT',
-      packages=['didgen'],
+      packages=find_packages(),
       python_requires='>=3',
       install_requires=[
           'numpy',
