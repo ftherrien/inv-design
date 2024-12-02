43df2d8311bd032a084d940a73e13206f96f8a1c
Felix Therrien <felix.therrien@gmail.com>
2024-12-02 02:12:23 -0500

    Added new models from Geom3D, GIN is the only fully implemented one

---
+++ tests/test_geom3d_models.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+# CH4
+adj_CH4 = torch.tensor([[0,1,1,1,1],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0]], dtype=torch.float)
+edge_index_CH4 = torch.tensor([[0,1],[0,2],[0,3],[0,4],[1,0],[2,0],[3,0],[4,0]], dtype=torch.long).T
+edge_attr_CH4 = torch.tensor([[1],[1],[1],[1],[1],[1],[1],[1]], dtype=torch.float)
+x_CH4 = torch.tensor([[0,1,0,0,0], [1,0,0,0,0], [1,0,0,0,0], [1,0,0,0,0], [1,0,0,0,0]], dtype=torch.float)
+
+# N2
+adj_N2 = torch.tensor([[0,3,0,0,0],[3,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]], dtype=torch.float)
+edge_index_N2 = torch.tensor([[0,1],[1,0]], dtype=torch.long).T
+edge_attr_N2 = torch.tensor([[3],[3]], dtype=torch.float)
+x_N2 = torch.tensor([[0,0,1,0,0], [0,0,1,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]], dtype=torch.float)
+
+def test_GINConv():
+    from didgen.models.geom3d_models import GINConv, GINConv_orig
+
+    emb_dim = 10
+
+    conv = GINConv(emb_dim, seed=0)
+    conv_orig = GINConv_orig(emb_dim, seed=0)
+    fea_emb = nn.Linear(5, emb_dim)
+    
+    out = conv(fea_emb(x_N2), adj_N2)
+    out_orig = conv_orig(fea_emb(x_N2), edge_index_N2, edge_attr_N2)
+
+    assert (out == out_orig).all(), "Failed to match the original implementation with N2"
+
+    out = conv(fea_emb(x_CH4), adj_CH4)
+    out_orig = conv_orig(fea_emb(x_CH4), edge_index_CH4, edge_attr_CH4)
+
+    assert (out == out_orig).all(), "Failed to match the original implementation with CH4"
+
+def test_Geom3D():
+    from didgen.models.geom3d_models import Geom3D
+
+    model = Geom3D(5, 3, 10, JK="last", drop_ratio=0, gnn_type="GIN", graph_pooling="mean", num_tasks=1)
+
+    adj = torch.stack([adj_N2, adj_CH4])
+    fea = torch.stack([x_N2, x_CH4])
+
+    out = model(fea, adj)
+    
+if __name__ == "__main__":
+    test_GINConv()
+    
+
+    
